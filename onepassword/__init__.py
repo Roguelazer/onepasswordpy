a325b7df175f1983f8944eba685c2c021a7398bc
James Brown <Roguelazer@gmail.com>
2013-02-10 13:17:40 -0800

    change version number and such

--- onepassword/__init__.py
+++ onepassword/__init__.py
@@ -1,6 +1,6 @@
 from .keychain import Keychain
 
-version_info = (0, 0, 1)
+version_info = (0, 0, 2)
 
-__author__ = 'James Brown <jbrown@yelp.com>'
+__author__ = 'James Brown <Roguelazer@gmail.com>'
 __version__ = '.'.join(map(str, version_info))
